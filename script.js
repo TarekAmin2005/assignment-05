61255216ff648c25ccfeeaaad942cd478f7175cc
TarekAmin2005 <tarekaminprof@gmail.com>
2026-03-09 13:48:14 +0600

    login js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Project Started");
+
+// LOGIN
+
+function login(){
+
+const user = document.getElementById("username").value;
+const pass = document.getElementById("password").value;
+
+if(user==="admin" && pass==="admin123"){
+
+document.getElementById("loginPage").classList.add("hidden");
+document.getElementById("mainPage").classList.remove("hidden");
+
+loadIssues();
+
+}else{
+
+alert("Invalid Credentials");
+
+}
+
+}
