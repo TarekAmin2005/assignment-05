0d7326cefe8a871e6f417e1ff0fba3fe110b94b0
TarekAmin2005 <tarekaminprof@gmail.com>
2026-03-09 14:52:52 +0600

    Issue And Loader Added

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 console.log("Project Started");
+const API = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+let allIssues = [];
 
 // LOGIN
 
@@ -19,5 +22,23 @@ loadIssues();
 alert("Invalid Credentials");
 
 }
+
+}
+
+
+// LOAD ISSUES
+
+async function loadIssues(){
+
+document.getElementById("loader").classList.remove("hidden");
+
+const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+const data = await res.json();
+
+allIssues = data.data;
+
+displayIssues(allIssues);
+
+document.getElementById("loader").classList.add("hidden");
 
 }
